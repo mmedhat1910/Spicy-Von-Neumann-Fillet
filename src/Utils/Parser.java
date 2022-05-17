15965a20dfef0001da58e774587bc434d43aa2dd
HazemSoliman9 <hazemsamehhh@gmail.com>
2022-05-17 22:52:37 +0200

    added code text file initiated parser class added loadCodeText function added parse function

--- src/Utils/Parser.java
+++ src/Utils/Parser.java
@@ -11,7 +11,50 @@ public class Parser {
     public Parser() throws IOException {
         this.codeText = loadCodeText("code.txt");
     }
+    public int parse(String codeline){
+        String[] instruction = codeline.split(" ");
+        int instructionValue = 0;
+        if(instruction[0].equals("J")){
+            instructionValue = 0b011100000000000000000000000000000;;
+            return instructionValue | Integer.parseInt(instruction[1]);
+        }
+        boolean addR3 = false;
+        instructionValue = switch (instruction[0]) {
+            // R types
+            case "SUB" -> 0b0001;
+            case "SLL" -> 0b1000;
+            case "SRL" -> 0b1001;
+            // I types
+            case "MULI" -> 0b0010;
+            case "ADDI" -> 0b0011;
+            case "BNE" -> 0b0100;
+            case "ANDI" -> 0b0101;
+            case "ORI" -> 0b0110;
+            case "LW" -> 0b1010;
+            case "SW" -> 0b1011;
+            default -> instructionValue;
+        };
+        //shift opcode
+        instructionValue = instructionValue << 26;
 
+        //R1
+        int tempReg =  Integer.parseInt(instruction[1].substring(1));
+        tempReg = tempReg << 22;
+        instructionValue = instructionValue | tempReg;
+        //R2
+        tempReg =  Integer.parseInt(instruction[2].substring(1));
+        tempReg = tempReg << 17;
+        instructionValue = instructionValue | tempReg;
+
+        if(instruction[0].equals("ADD") || instruction[0].equals("SUB")){
+            int r3 =  Integer.parseInt(instruction[3].substring(1));
+            r3 = r3 << 12;
+            return instructionValue | r3;
+        }
+        //shamt or imm
+        return instructionValue | Integer.parseInt(instruction[3]);
+
+        }
     public ArrayList<String> loadCodeText(String filename) throws IOException {
         ArrayList<String> code = new ArrayList<>();
         BufferedReader br = new BufferedReader(new FileReader(filename));
@@ -31,5 +74,10 @@ public class Parser {
     public static void main(String[] args) throws IOException {
         Parser p = new Parser();
         System.out.println(p.getCodeText());
+        p.parse(p.getCodeText().get(0));
+        for (String inst : p.getCodeText()){
+            System.out.println( Integer.toBinaryString(p.parse(inst)));
+        }
+
     }
 }

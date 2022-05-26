a7d9c261506de0eafabe7f27b4440942ab9c0713
Mohamed Medhat <mmedhat1910@gmail.com>
2022-05-26 22:46:19 +0200

    passing: memory

---
+++ src/Components/MainMemoryTest.java
@@ -0,0 +1,34 @@
+package Components;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MainMemoryTest {
+
+    @Test
+    public void testGetInstruction() {
+        MainMemory memory = new MainMemory();
+        memory.setAddress(0);
+        memory.writeData(0);
+        assertEquals(0, memory.getInstruction());
+
+        memory.setAddress(500);
+        memory.writeData(5210);
+        assertEquals(5210, memory.getInstruction());
+    }
+
+    @Test
+    public void getReadWriteData() {
+        MainMemory memory = new MainMemory();
+        memory.setMemWrite(true);
+        memory.setAddress(2000);
+        memory.writeData(65489);
+        memory.setMemWrite(false);
+
+        memory.setMemRead(true);
+        memory.setAddress(2000);
+
+         assertEquals(65489, memory.getReadData());
+    }
+}
